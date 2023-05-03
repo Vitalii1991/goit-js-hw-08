16603bf085e4f61f570bee7bca0894651662a4b6
Vitalii1991 <39694318+Vitalii1991@users.noreply.github.com>
2023-05-03 14:30:41 +0000

    Deploying to gh-pages from @ Vitalii1991/goit-js-hw-08@06839b8cef60f1d2e460422d29ad10e44898b579 🚀

--- 02-video.4c616415.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector("iframe"),o=new Vimeo.Player(e);o.on("play",(function(){console.log("played the video!")})),o.getVideoTitle().then((function(e){console.log("title:",e)})),o.setVolume(0);
-//# sourceMappingURL=02-video.4c616415.js.map
