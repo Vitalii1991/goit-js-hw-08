ba9cd3595e54ac8940646ad647c58c9c02e52d4a
Vitalii1991 <ironwarrior1991@gmail.com>
2023-05-08 13:32:55 +0300

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -6,30 +6,19 @@ const player = new Player(iframe);
 player.on('timeupdate', onPlay);
 
 function onPlay(data) {
-  console.log('played the video!');
-
-  const videoStopTime = data.seconds;
-  console.log(videoStopTime);
+  const currentTime = data.seconds;
+  localStorage.setItem('videoplayer-current-time', currentTime);
 }
 
-// localStorage.setItem('videoplayer-current-time', 0);
-// const timePlayer = localStorage.getItem('videoplayer-current-time');
+const timeLocalStorage = localStorage.getItem('videoplayer-current-time');
 
-player
-  .setCurrentTime(25)
-  .then(function () {
-    // seconds = the actual time that the player seeked to
-  })
-  .catch(function (error) {
-    switch (error.name) {
-      case 'RangeError':
-        // the time was less than 0 or greater than the video’s duration
-        break;
-
-      default:
-        // some other error occurred
-        break;
-    }
-  });
+player.setCurrentTime(timeLocalStorage).catch(function (error) {
+  switch (error.name) {
+    case 'RangeError':
+      break;
+    default:
+      break;
+  }
+});
 
 player.setVolume(0.1);
