ed76c01237987080d161c7b258ab03cbed819471
Vitalii1991 <ironwarrior1991@gmail.com>
2023-05-08 13:07:48 +0300

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,33 +3,33 @@ import Player from '@vimeo/player';
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
-player.on('play', onPlayVideo);
+player.on('timeupdate', onPlay);
 
-function onPlayVideo(event) {
+function onPlay(data) {
   console.log('played the video!');
 
-  const videoStopTime = event.seconds;
+  const videoStopTime = data.seconds;
   console.log(videoStopTime);
 }
 
-localStorage.setItem('videoplayer-current-time', 30);
-const timePlayer = localStorage.getItem('videoplayer-current-time');
+// localStorage.setItem('videoplayer-current-time', 0);
+// const timePlayer = localStorage.getItem('videoplayer-current-time');
 
-player
-  .setCurrentTime(timePlayer)
-  .then(function () {
-    // seconds = the actual time that the player seeked to
-  })
-  .catch(function (error) {
-    switch (error.name) {
-      case 'RangeError':
-        // the time was less than 0 or greater than the video’s duration
-        break;
+// player
+//   .setCurrentTime(timePlayer)
+//   .then(function () {
+//     // seconds = the actual time that the player seeked to
+//   })
+//   .catch(function (error) {
+//     switch (error.name) {
+//       case 'RangeError':
+//         // the time was less than 0 or greater than the video’s duration
+//         break;
 
-      default:
-        // some other error occurred
-        break;
-    }
-  });
+//       default:
+//         // some other error occurred
+//         break;
+//     }
+//   });
 
 player.setVolume(0.1);
